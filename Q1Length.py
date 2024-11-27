0f9143846901d13aff4a449b732974f57d7b5af8
Junwon Choi <junwon.choi@stu.ocsb.ca>
2024-11-27 15:14:47 +0000

    e

--- Q1Length.py
+++ Q1Length.py
@@ -1,2 +1,6 @@
-# Create a program that will accept a word from a user and return the length of that word. 
-# Make this program in a loop with option to exit when the use types in quit.
+while True:
+    word = input()
+    if word == 'quit':
+        break
+    else:
+        print(len(word))
